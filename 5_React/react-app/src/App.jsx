fcdb86c2201e94d29bb7e441d9b3843b0938ce2f
wjdguswhd <guswhd43@naver.com>
2026-03-23 12:52:15 +0900

    update

---
+++ 5_React/react-app/src/App.jsx
@@ -0,0 +1,7 @@
+import funcComp from './components/01_component/FuncComponent';
+
+function App() {
+    return<funcComp/> //한 줄 밖에 없으므로 소괄호 생략가능
+}
+
+export default App
---
+++ 5_React/react-app/src/components/01_component/FuncComponent.jsx
@@ -0,0 +1,5 @@
+const test = () => {
+    return <div>함수형 컴포넌트입니다.</div> //한 줄 밖에 없으므로 소괄호 생략가능
+}
+
+export default test;
